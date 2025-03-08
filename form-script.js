fa286d1f0a5d35e66d6f088b602c83a5849447b1
FirdausIqbal <firdausiqbal10@gmail.com>
2025-03-08 14:12:01 +0700

    firs-commit

--- form-script.js
+++ form-script.js
@@ -1,10 +1,34 @@
-(() => {
-  const API_URL = 'http://localhost:3000/employees';
-
+(async() => {
   /**
    * TODO:
    * - Add a function to generate random string (10 digits alphanumeric) for id
    * - Add a function to add new data (submit)
    * - Add a function to redirect to the index page
    */
+  const API_URL = 'http://localhost:3000/employees';
+  const form = document.getElementById('data-form');
+  const name = document.getElementById('name');
+  const position = document.getElementById('position');
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const data = {
+      id: Math.random().toString(36).substr(2, 10),
+      name: name.value,
+      position: position.value
+    };
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    if (response.ok) {
+      window.location.href = 'index.html';
+    } else {
+      console.error('Failed to add data');
+      alert('Failed to add data');
+    }
+  });
 })();
--- index-script.js
+++ index-script.js
@@ -1,5 +1,9 @@
-(() => {
+(async() => {
   const API_URL = 'http://localhost:3000/employees';
+  const tableData = document.getElementById('data-table');
+  const searchBar = document.getElementById("search");
+  const deleteBtn = document.getElementById("btndelete");
+
 
   /**
    * TODO:
@@ -9,4 +13,56 @@
    * - Add a function to display table
    * - Add a function to delete data (bonus)
    */
+  try {
+    const fetchData = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      return data;
+    }
+    const updateTable = (data) => {
+      data.forEach(employee => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+          <td>${employee.id}</td>
+          <td>${employee.name}</td>
+          <td>${employee.position}</td>
+          <button id="btndelete" value=${employee.id}>Delete</button>
+        `;
+        tableData.appendChild(tr);
+      });
+    }
+    
+    const data = await fetchData();
+    updateTable(data);
+
+    // Tangkap event saat user input di searchbar
+    searchBar.addEventListener("keyup", (e) => {
+      const input = e.target.value;
+      const searchData = data.filter(item => item.name.includes(input));
+      tableData.innerHTML = '';
+      updateTable(searchData);
+    });
+
+    // Tangkap event click Button #delete
+    tableData.addEventListener("click", async (e) => {
+      e.preventDefault();
+      if(e.target.id === "btndelete") {
+        try {
+          const id = e.target.value;
+          const response = await fetch(`${API_URL}/${id}`, {
+            method: 'DELETE'
+          });
+          const data = await response.json();
+          alert(`Data ${data.name} berhasil dihapus`);
+        } catch (error) {
+          console.log(error.message);
+          alert(error.message);
+        }
+      }
+    })
+
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+  }
 })();
